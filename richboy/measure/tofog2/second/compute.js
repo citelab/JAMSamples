64abc8790afa0160a030cba556634da92cd07cd0
richboy <david.echomgbe@gmail.com>
2018-05-16 03:29:07 -0400

    measurement app update

--- richboy/measure/tofog2/second/compute.js
+++ richboy/measure/tofog2/second/compute.js
@@ -3,8 +3,11 @@ jdata{
 }
 
 var microtime = require('microtime');
+var fs = require('fs');
 
 var handle = new InFlow(JAMManager);
+
+var ws = fs.createWriteStream("output.txt", {flags:'a'});
 
 handle.setTerminalFunction(function(input){
     //console.log(input);
@@ -16,6 +19,7 @@ handle.setTerminalFunction(function(input){
         var microC = logParts[0] * 1000000 + (logParts[1] - 0);
 
         console.log("ExecTime,", input, ",", microJ - microC, "µs");
+        ws.write("ExecTime," + input + "," + (microJ - microC) + "µs" + "\n");
     }
 });
 
