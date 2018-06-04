bd970d5a2aeb109a2bfab72f230cedcf0db5291f
richboy <david.echomgbe@gmail.com>
2018-06-03 23:03:03 -0400

    hello app update

--- richboy/hello/hello.js
+++ richboy/hello/hello.js
@@ -6,7 +6,7 @@
 
 jdata{
     int x as logger;
-    f as flow with toDiscretizer of x;
+    //f as flow with toDiscretizer of x;
 }
 
 function toDiscretizer(inputFlow){
@@ -22,14 +22,26 @@ var terminalFunc = discreteFlow => {
     console.log("Sum: " + sum + ", Average: " + avg);
 };
 
-f.setTerminalFunction(terminalFunc);
+
 
 //poll until we have up to 3 C-Nodes running
 (function poll(){
-    if( x.size() < 3 ){
+    if( x.size() < 4 ){
         console.log("waiting till we have 3 C-nodes running");
         setTimeout(poll, 2000);
     }
-    else
+    else {
+        var myFlow = null;
+        for(var i = 0; i < x.size(); i++){
+            if( x[i].key === x.getMyDataStream().key )
+                continue;
+            if( myFlow == null )
+                myFlow = Flow.from(x[i]);
+            else
+                myFlow.merge(x[i]);
+        }
+        var f = toDiscretizer(myFlow);
+        f.setTerminalFunction(terminalFunc);
         f.startPush();
+    }
 })();
