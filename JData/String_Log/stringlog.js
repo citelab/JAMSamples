0255a9dc6e39dd7cb4abf7e220a677cdd9d20403
anrl <mahes25@gmail.com>
2018-06-14 11:44:17 -0400

    Coordinate support

--- JData/String_Log/stringlog.js
+++ JData/String_Log/stringlog.js
@@ -9,12 +9,15 @@ var count = 1;
 setInterval(function() {
 
 	console.log("Size of logger ", name.size());
+    console.log("Longitude ", jsys.long);
+    console.log("Lattitude ", jsys.lat);
+    console.log("Link strategy ", jsys.link);
 
 	if (jsys.type === "cloud")
 	    nlogger.log("fred@cloud-" + count);
 	else if (jsys.type === "fog")
 	    nlogger.log("fred@fog-" + count);
-	else 
+	else
 	    nlogger.log("fred@device-" + count);
 
 	for (i = 0; i < name.size(); i++) {
